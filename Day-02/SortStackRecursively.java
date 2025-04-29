9ca8d411c5e0bc679f371e3499bb52501983bc6c
[Kanaka Srinidhi] <[bkanakasrinidhi@gmail.com]>
2025-04-29 14:25:49 +0530

    day 2

---
+++ Day-02/SortStackRecursively.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+public class SortStackRecursively {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+
+        System.out.println("Original Stack: " + stack);
+
+        sortStack(stack);
+
+        System.out.println("Sorted Stack: " + stack);
+    }
+
+    static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int temp = stack.pop();
+            sortStack(stack);
+            insertInSortedOrder(stack, temp);
+        }
+    }
+
+    static void insertInSortedOrder(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || stack.peek() <= element) {
+            stack.push(element);
+        } else {
+            int temp = stack.pop();
+            insertInSortedOrder(stack, element);
+            stack.push(temp);
+        }
+    }
+}
